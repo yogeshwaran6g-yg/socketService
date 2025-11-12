c6f670a6059dc7d5dddbd6339aa1ee3afac4738c
yogeshwaran6g-yg <yogeshwaran6g@gmail.com>
2025-11-12 10:48:09 +0530

    added socket test file

---
+++ src/nodeClientTest.js
@@ -0,0 +1,22 @@
+const { io } = require("socket.io-client");
+
+const socket = io("http://localhost:4000", {
+  transports: ["websocket"],
+});
+
+socket.on("connect", () => {
+  console.log("✅ Connected:", socket.id);
+
+  // Example emits
+  socket.emit("joinRoom", { roomName: "TigerDragon", playerId: 1, "username": "PlayerOne"});
+
+  socket.emit("placeBet", { match_uuid: "abc123", player_id: 1, username: "PlayerOne", clan_name: "Tiger", bet_amount: 100 });
+});
+
+socket.onAny((event, data) => {
+  console.log("📨 Server event:", event, data);
+});
+
+
+//  playerId, username, roomName
+// { match_uuid, player_id, username, clan_name, bet_amount } = data;
