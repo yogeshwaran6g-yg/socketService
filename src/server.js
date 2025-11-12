bb711340a9c1cacacbae6a5cf44e9e834306c923
yogeshwaran6g-yg <yogeshwaran6g@gmail.com>
2025-11-12 10:47:30 +0530

    added express server

---
+++ src/server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const cors = require("cors");
+const helmet = require("helmet");
+const compression = require("compression");
+const morgan = require("morgan");
+const http = require("http");
+const { initSocket } = require("./socket/socketController");
+//middlewares
+const notFound = require("./middleware/notFound");
+const errorHandler = require("./middleware/errHandler");
+require("dotenv").config();
+//job
+const {startMatchScheduler}=require("./jobs/matchJob")
+
+const app = express();
+app.use(helmet());
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+  })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(compression());
+app.use(morgan("dev"));
+
+-
+app.get("/", (req, res) => {
+  return res.status(200).json({ msg: "🚀 Welcome to Socket.IO Service" });
+});
+
+app.use(notFound);
+app.use(errorHandler);
+
+// --- HTTP Server & Socket.IO ---
+const PORT = process.env.PORT || 4000;
+const server = http.createServer(app);
+
+// Initialize Socket.IO
+initSocket(server);
+startMatchScheduler();
+
+// Start server
+server.listen(PORT, () => {
+  console.log(`✅ Server running on http://localhost:${PORT}`);
+});
